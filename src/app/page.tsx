2566255d623b92c580f213bfd12724c186385e1a
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2025-03-11 05:24:26 +0100

    chore: set up shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default async function Home() {
   return (
     <div className="">
       <h1 className="">hello boluwatife</h1>
+      <Button>click me</Button>
     </div>
   );
 }
