9f547769b141674cee38d4ec319f0a590b6c07ef
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2025-03-12 12:26:45 +0100

    feat: setup aurinko integration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import { Button } from "@/components/ui/button";
+import LinkAccountButton from "@/components/LinkAccountButton";
 
 export default async function Home() {
   return (
     <div className="">
       <h1 className="">hello boluwatife</h1>
-      <Button>click me</Button>
+      <LinkAccountButton />
     </div>
   );
 }
