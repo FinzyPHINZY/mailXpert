f93c6d49f920ddaeba77eb3074a5e385b7aa6827
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2025-03-12 12:31:33 +0100

    chore: created mail page

---
+++ src/app/mail/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MailDashoard = () => {
+  return (
+    <div>
+      <p className="">this is the mail dashboard</p>
+    </div>
+  );
+};
+
+export default MailDashoard;
